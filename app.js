cc0dbd97d3496232079ddfefb405e063a856a971
Achyut Paudel <achyut.pdl@gmail.com>
2016-01-12 11:12:33 -0600

    Git ignore updated

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var finalhandler = require('finalhandler')
-var http         = require('http')
-var Router      = require('router')
-var moment = require('moment');
-require('moment-range');
-var fs = require('fs')
-var jsdom = require('jsdom')
-, request = require('request')
-, url = require('url')
+var finalhandler    = require('finalhandler')
+var http            = require('http')
+var Router          = require('router')
+var moment          = require('moment');
+var range           = require('moment-range');
+var fs              = require('fs');
+var jsdom           = require('jsdom');
+var request         = require('request');
+var url             = require('url');
 
 var router = Router()
 
@@ -16,9 +16,25 @@ var dates = getDateRange();
 
 var queue = [];
 var totalGames = 0;
-//queue.push(2);         // queue is now [2]
-//queue.push(5);         // queue is now [2, 5]
-//var i = queue.shift(); // queue is now [5]
+
+
+router.get('/', function (req, res) {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.setHeader('Connection', 'Transfer-Encoding');
+  res.setHeader('Transfer-Encoding', 'chunked');
+  getNextData(res,"");
+})
+
+router.get('/parsedata', function (req, res) {
+  fs.readFile('./parser.html', function (err, html) {
+        if (err) {
+            throw err; 
+        } 
+        res.writeHeader(200, {"Content-Type": "text/html"});  
+        res.write(html);  
+        res.end();
+    });
+})
 
 function getDateRange(){
     var startdate = moment('2016-01-01');
@@ -30,34 +46,6 @@ function getDateRange(){
   }
   return dates;    
 }
-
-
-router.get('/', function (req, res) {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.setHeader('Connection', 'Transfer-Encoding');
-  res.setHeader('Transfer-Encoding', 'chunked');
- // console.log(dates);
-  //getScrapInInterval(dates,res);
-  getNextData(res,"");
-
-  /*if(dates.length>0){
-    getGameLinks(dates.shift(),res);  
-  }*/
-  //getGameDetailLinks(date,res);
-  //res.end("Done");
-})
-
-function getScrapInInterval($dates,res){
-  var c = 0;
-  var timeout = setInterval(function() {
-    getGameLinks($dates[0],res);
-    c++;
-    if (c > $dates.length) {
-      clearInterval(timeout);
-    }
-  }, waitTime);
-}
-
 
 function getGameLinks($date,res){
     //var $date = "20160107";
@@ -93,8 +81,7 @@ function parseDetailLinks(window,res,$date){
             link ='http://nba.com'+link;
             queue.push(link);
             totalGames++;
-        });
-        //getNextData(res,$date);    
+        });   
     }
     else{
         res.write("..Games not found for the day.");
@@ -119,8 +106,8 @@ function getNextData(res,$date){
       
     }, waitTime);
 }
+
 function getDataForEachGame($url,res,$date){
-    //console.log($url);
     if($url == undefined){
         return;
     }
@@ -133,18 +120,14 @@ function getDataForEachGame($url,res,$date){
     },
     uri: $url}, function(err, response, body){
         var self = this;
-        self.items = new Array();//I feel like I want to save my results in an array
-        //Just a basic error check
+        self.items = new Array();
         if(err && response.statusCode !== 200){console.log('Request error.');}
 
         jsdom.env({
             html: body,
             scripts: ['http://code.jquery.com/jquery-1.6.min.js'],
             done: function (err, window) {
-                    res.write("..Data downloaded for the game...");
-                    //res.send($('div#nbaGIPBP').text());
-                    //res.send($('title').text());
-                    //extractDataFromPage(window,res,$date,$filename);
+                    res.write("...Data downloaded for the game...");
                     extractTableFromPage(window,res,$date,$filename);
                 }
             });
@@ -154,65 +137,60 @@ function getDataForEachGame($url,res,$date){
 function extractTableFromPage(window,res,$date,$filename){
     res.write("extracting play by play data...");
     var $ = window.jQuery;
-    $body = $('body')
-    $table = $body.find('div#nbaGIPBP table')[0].outerHTML;
-    $filename = 'htmldata/'+$filename+'.html';
-    fs.writeFile($filename,$table, function (err) {
-      if (err) return console.log(err);
-      res.write("Data written into the file.."+$filename+"...");
-      console.log('Data written for file:'+$filename);
-      res.write("Done!!!");
-      //res.write("<hr>");
-      //getNextData(res,$date);
-    });
-    
+    $body = $('body');
+    if($body.find('div#nbaGIPBP table')[0]!==undefined){
+      $table = $body.find('div#nbaGIPBP table')[0].outerHTML;
+      $filename = 'htmldata/'+$filename+'.html';
+      fs.writeFile($filename,$table, function (err) {
+        if (err) return console.log(err);
+        res.write("Data written into the file.."+$filename+"...");
+        console.log('Data written for file:'+$filename);
+        res.write("Done!!!");
+        //res.write("<hr>");
+        //getNextData(res,$date);
+      }); 
+    }
+    else{
+        totalGames--;
+        res.write("Data not found for the day.");
+    }
+       
 }
 
 function extractDataFromPage(window,res,$date,$filename){
-//Use jQuery just as in a regular HTML page
     var dta = [];
     var $ = window.jQuery;
     $body = $('body')
-    $tr = $body.find('div#nbaGIPBP table tr');
-    $tr.each(function (i, item) {
-        var data1 = $(item).text();
-            //data1 = data1.replace(/,/g , '').replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-            var lines = data1.split(/\n/);
-            var newlines = [];
-            $.each(lines,function(i,item1){
-                newlines.push($.trim(item1));
-            });
-            newlines = newlines.filter(Boolean)
-            
-            //lines = lines.filter(Boolean)
-            $left = $(item).children('td.nbaGIPbPLft,td.nbaGIPbPLftScore,div.gameEvent');
-            $mid = $(item).children('td.nbaGIPbPMid,td.nbaGIPbPMidScore');
-            $right = $(item).children('td.nbaGIPbPRgt,td.nbaGIPbPRgtScore');
-            dta.push(newlines);
-        });
-    fs.writeFile('data/'+$filename+'.json',JSON.stringify(dta), function (err) {
-      if (err) return console.log(err);
-      console.log('Data written for filename:'+$filename);
-      res.write("Done!!!");
-      res.write("<hr>");
-    });
-    //console.log(self.items);
-    
+    if($body.find('div#nbaGIPBP table')[0]!==undefined){
+      $tr = $body.find('div#nbaGIPBP table tr');
+      $tr.each(function (i, item) {
+          var data1 = $(item).text();
+              //data1 = data1.replace(/,/g , '').replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+              var lines = data1.split(/\n/);
+              var newlines = [];
+              $.each(lines,function(i,item1){
+                  newlines.push($.trim(item1));
+              });
+              newlines = newlines.filter(Boolean)
+              
+              //lines = lines.filter(Boolean)
+              $left = $(item).children('td.nbaGIPbPLft,td.nbaGIPbPLftScore,div.gameEvent');
+              $mid = $(item).children('td.nbaGIPbPMid,td.nbaGIPbPMidScore');
+              $right = $(item).children('td.nbaGIPbPRgt,td.nbaGIPbPRgtScore');
+              dta.push(newlines);
+          });
+      fs.writeFile('data/'+$filename+'.json',JSON.stringify(dta), function (err) {
+        if (err) return console.log(err);
+        console.log('Data written for filename:'+$filename);
+        res.write("Done!!!");
+        res.write("<hr>");
+      });
+    }
+    else{
+      totalGames--;
+      res.write("Data not found for the day."); 
+    }
 }
-
-
-router.get('/parsedata', function (req, res) {
-  fs.readFile('./parser.html', function (err, html) {
-        if (err) {
-            throw err; 
-        } 
-        res.writeHeader(200, {"Content-Type": "text/html"});  
-        res.write(html);  
-        res.end();
-
-    });
-})
-
 
 var server = http.createServer(function(req, res) {
   router(req, res, finalhandler(req, res))
